11528d031fc927afab364edceb6176fe4d11c794
ronaldo cisneros valencia <cisnerosronaldo12@gmail.com>
2023-04-02 21:00:31 -0600

    nuevo

--- js/noticia.js
+++ js/noticia.js
@@ -94,3 +94,17 @@ function siguiente(){
 }
 
 noticias.fetchNoticias(temaActual);
+
+function enableDarkMode() {
+    let main_body = document.body;
+    main_body.classList.toggle("dark-mode");
+
+    // Check if checkbox is checked or not
+    if (document.getElementById('checkitem').checked) {
+        console.log("checked");
+    } else {
+        console.log("NOT checked");
+    }
+
+
+}
